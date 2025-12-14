93b39a29952f885123d30dfc5bc97a8c1ffb1e36
SanjayDoss <sanjaymurugadosscsbs02@gmail.com>
2025-12-14 05:32:37 +0530

    final

--- server_socket.py
+++ server_socket.py
@@ -81,4 +81,10 @@ def on_disconnect():
 # ---------------- RUN SERVER ----------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(
+    app,
+    host="0.0.0.0",
+    port=port,
+    allow_unsafe_werkzeug=True
+    )
+
